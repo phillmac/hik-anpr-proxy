4cfa0c515f4fd6f60249fa18e5afc23f70e7bab3
phillmac <phillmackintosh@gmail.com>
2024-02-16 03:55:06 +0000

    Add server.js

---
+++ httpParser.js
@@ -0,0 +1,35 @@
+// httpParser.js
+
+const { EventEmitter } = require('events');
+
+class HTTPParser extends EventEmitter {
+  constructor() {
+    super();
+    this.buffer = Buffer.alloc(0);
+    this.headersComplete = false;
+  }
+
+  parse(data) {
+    this.buffer = Buffer.concat([this.buffer, Buffer.from(data)]);
+    this.processBuffer();
+  }
+
+  processBuffer() {
+    const headerEnd = this.buffer.indexOf('\r\n\r\n');
+
+    if (this.headersComplete === false && headerEnd !== -1) {
+      const headers = this.buffer.subarray(0, headerEnd).toString();
+      this.emit('headers', headers);
+
+      this.buffer = this.buffer.subarray(headerEnd + 4);
+      this.headersComplete = true;
+    }
+
+    if(this.headersComplete && Buffer.byteLength(this.buffer > 0)) {
+      this.emit('data', this.buffer);
+      this.buffer = Buffer.alloc(0);
+    }
+  }
+}
+
+module.exports = HTTPParser;
---
+++ server.js
@@ -0,0 +1,94 @@
+// server.js
+
+const net = require('net');
+const { URL } = require('url'); // Import the URL module
+const HTTPParser = require('./httpParser');
+const http = require('http');
+
+
+const UPSTREAM_SERVER_HOST = 'hubtest.siteguard.online';
+const UPSTREAM_SERVER_PORT = 80
+
+
+const server = net.createServer();
+
+// Handle client connection
+server.on('connection', (socket) => {
+  console.log('New client connected.');
+
+  const parser = new HTTPParser();
+
+  let method, httpVersion, urlObject, queryParams, body;
+
+  const headers = new Headers();
+
+  // Handle incoming data for each client
+  socket.on('data', (data) => {
+    parser.parse(data.toString());
+  });
+
+  // Handle parsed headers
+  parser.on('headers', (receivedHeaders) => {
+    // Process headers for this client
+    // console.log('Received headers:', receivedHeaders);
+
+    const headersList = receivedHeaders.split('\r\n');
+
+    const requestLine = headersList.shift(); // Assuming the first line is the request line
+
+    for (const h of headersList) {
+      headers.append(... h.split(':'));
+    }
+
+    // Extract and parse query parameters from the request URL
+    const [m, ...urlParts] = requestLine.split(' ');
+
+    method = m;
+
+    if (urlParts.length > 0) {
+      // Remove the httpVersion before joining
+      httpVersion = urlParts.pop();
+
+      // Join URL parts with an encoded space
+      // const url = urlParts.map(encodeURIComponent).join('%20');
+      const url = urlParts.join('%20');
+
+      urlObject = new URL(url, `http://${UPSTREAM_SERVER_HOST}:${UPSTREAM_SERVER_PORT}`);
+      queryParams = urlObject.searchParams;
+
+      console.log(JSON.stringify({ method, httpVersion, path: urlObject.pathname, query: queryParams.entries()}));
+
+    }
+  });
+
+  // Handle body data
+  parser.on('data', (d) => {
+    body = Buffer.concat([body, d]);
+  });
+
+  // Handle socket closure
+  socket.on('end', () => {
+    if(socket.writable) {
+      socket.write(`${httpVersion} 200 OK\r\n\r\n"OK"\r\n`);
+    }
+
+    console.log('Client disconnected.');
+
+     if(method = 'POST') {
+      fetch(urlObject, {
+        method,
+        headers,
+        body
+      })
+        .then((result) => console.log(result.ok))
+        .catch(err => console.error(err));
+     }
+  });
+});
+
+
+// Listen on port 3002
+const PORT = 3002;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
